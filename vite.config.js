7638212ced1f64473486b18ca9d7ef7daefba58c
Purushendra Karn <purushendrakarn@gmail.com>
2025-11-07 21:45:41 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Purushendra-Portfolio/"
+  base: "", // ✅ relative paths, works on root & subpath
 });
